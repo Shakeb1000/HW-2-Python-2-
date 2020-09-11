a234f18b563e32af23326f8aaae2a083fe4e0438
Shakeb Siddiqui <sms8508@psu.edu>
2020-09-11 23:57:22 +0000

    oefw

--- main.py
+++ main.py
@@ -25,25 +25,27 @@ def getGradePoint(grade):
  return course1gp
 
 
+def mainfunction():
+  grade1 = input("Enter your course 1 letter grade: ")
+  Gp1 = getGradePoint(grade1)
+  credit1 = input("Enter your course 1 credit: ")
+  print (f"Grade point for course 1 is: {Gp1}")
 
-grade1 = input("Enter your course 1 letter grade: ")
-Gp1 = getGradePoint(grade1)
-credit1 = input("Enter your course 1 credit: ")
-print (f"Grade point for course 1 is: {Gp1}")
+  grade2 = input("Enter your course 2 letter grade: ")
+  Gp2 = getGradePoint(grade2)
+  credit2 = input("Enter your course 2 credit: ")
+  print (f"Grade point for course 2 is: {Gp2}")
 
-grade2 = input("Enter your course 2 letter grade: ")
-Gp2 = getGradePoint(grade2)
-credit2 = input("Enter your course 2 credit: ")
-print (f"Grade point for course 2 is: {Gp2}")
+  grade3 = input("Enter your course 3 letter grade: ")
+  Gp3 = getGradePoint(grade3)
+  credit3 = input("Enter your course 3 credit: ")
+  print (f"Grade point for course 3 is: {Gp3}")
 
-grade3 = input("Enter your course 3 letter grade: ")
-Gp3 = getGradePoint(grade3)
-credit3 = input("Enter your course 3 credit: ")
-print (f"Grade point for course 3 is: {Gp3}")
+  credit1=float(credit1)
+  credit2=float(credit2)
+  credit3=float(credit3)
 
-credit1=float(credit1)
-credit2=float(credit2)
-credit3=float(credit3)
+  GPA = ((Gp1 * credit1) + (Gp2 * credit2) + (Gp3 * credit3)) / (credit1 + credit2 + credit3)
+  print(f"Your GPA is: {GPA}")
 
-GPA = ((Gp1 * credit1) + (Gp2 * credit2) + (Gp3 * credit3)) / (credit1 + credit2 + credit3)
-print(f"Your GPA is: {GPA}")
+mainfunction()
